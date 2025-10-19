05d6d98d6d0e446972d8cc8629acfc69863f9cc2
Anitha Mzwakali <136008347+AnithaMzwakali@users.noreply.github.com>
2025-10-19 17:06:41 +0200

    Remove database creation function and add SQL query

--- task_4.sql
+++ task_4.sql
@@ -1,21 +1,10 @@
-import mysql.connector
-from mysql.connector import errorcode
-
-def create_database():
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            user='root',       # Replace with correct username
-            password='YOUR_PASSWORD',
-            host='127.0.0.1'
-        )
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
-    except mysql.connector.Error as err:
-        print(f"Error while connecting to MySQL: {err}")
-    finally:
-        if connection and connection.is_connected():
-            cursor.close()
-            connection.close()
-
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'Books';
